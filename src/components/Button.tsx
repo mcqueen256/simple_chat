36bc000c99e05f63496437982d0cf8a98a285b6f
Nicholas Buckeridge <bucknich@gmail.com>
2018-12-07 16:30:11 +1000

    🔘 Button stuff

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 
-export interface ButtonProps {buttonText: string;}
+export interface ButtonProps {
+    buttonText: string;
+    onClick: Function;
+}
 
 export class Button extends React.Component<ButtonProps, {}> {
     render(){
         return  <div>
-                    <button className='ui primary button' role='button'>
+                    <button className='ui primary button' role='button' onClick={event => this.props.onClick}>
                         {this.props.buttonText}
                     </button>
                 </div>
--- src/components/SettingsContainer.tsx
+++ src/components/SettingsContainer.tsx
@@ -1,12 +1,16 @@
 import * as React from "react";
-//import { Button } from "semantic-ui-react";
-import {Button} from "./Button";
-export class SettingsContainer extends React.Component<{}, {}> {
+import { Button } from "semantic-ui-react";
+// import {Button} from "./Button";
+interface SettingsProp {
+    connector: Function;
+}
+
+export class SettingsContainer extends React.Component<SettingsProp, {}> {
     render(){
         return <div>
                     <div>Ip textbox</div>
                     <div>Port textbox</div> 
-                    <Button buttonText="Connect"></Button>
+                    <Button content="Connect" onClick={event => this.props.connector}></Button>
                </div>
 
 }
